a1abc6c2ad64358c4f5fa8c019a8bc3abb93d089
William Bowden <william.j.bowden@aol.com>
2019-08-03 11:05:46 -0400

    Camera Movement before players join

--- Assets/TargetGroupManager.cs
+++ Assets/TargetGroupManager.cs
@@ -9,11 +9,42 @@ public class TargetGroupManager : MonoBehaviour {
     [SerializeField] CinemachineTargetGroup tg = null;
 
     int playerCount = 0;
+    float camTimer = 0;
+    int camPriority = 0;
+    int prevPriority = 0;
 
     // Start is called before the first frame update
     void Start() {
         if( !tg ) {
             tg = GameObject.Find( "TargetGroup1" ).GetComponent<CinemachineTargetGroup>();
+        }
+    }
+
+    private void Update() {
+        if( playerCount <= 0 ) {
+            camTimer += Time.deltaTime;
+
+            if( camTimer >= 5.0f ) {
+                //tg.m_Targets[ camPriority ].weight = 1f;
+
+                prevPriority = camPriority++;
+
+                if( camPriority > tg.m_Targets.Length - 1 ) {
+                    camPriority = 0;
+                }
+                camTimer = 0;
+            }
+
+            //tg.m_Targets[ camPriority ].weight += Time.deltaTime;
+            tg.m_Targets[ camPriority ].weight = Mathf.Lerp( tg.m_Targets[ camPriority ].weight, 5, 0.01f );
+
+            if( prevPriority != camPriority ) {
+                tg.m_Targets[ prevPriority ].weight = Mathf.Lerp( tg.m_Targets[ prevPriority ].weight, 1, 0.1f );
+            }
+
+            //foreach( CinemachineTargetGroup.Target target in tg.m_Targets ) {
+
+            //}
         }
     }
 
