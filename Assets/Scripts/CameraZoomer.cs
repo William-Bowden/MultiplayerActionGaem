e1efe87f7362485bd4a85a958cdd1e79e6b1693e
William-Bowden <william.j.bowden@aol.com>
2019-08-06 14:36:22 -0400

    Add camera zoom depending on distance from furthest two targetGroup targets. Add Famas

---
+++ Assets/Scripts/CameraZoomer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraZoomer : MonoBehaviour {
+
+    CinemachineVirtualCamera cam;
+
+    [SerializeField] CinemachineTargetGroup tg = null;
+
+    // Start is called before the first frame update
+    void Start() {
+        cam = GetComponent<CinemachineVirtualCamera>();
+
+        if( !tg ) {
+            tg = GameObject.Find( "TargetGroup1" ).GetComponent<CinemachineTargetGroup>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float furthest = 1;
+
+        if( tg.m_Targets.Length > 1 ) {
+            foreach( CinemachineTargetGroup.Target target1 in tg.m_Targets ) {
+                foreach( CinemachineTargetGroup.Target target2 in tg.m_Targets ) {
+                    float dist = Mathf.Abs( ( target1.target.position - target2.target.position ).magnitude );
+
+                    if( dist > furthest ) {
+                        furthest = dist;
+                    }
+                }
+            }
+
+
+        }
+
+        cam.m_Lens.OrthographicSize = Mathf.Lerp( cam.m_Lens.OrthographicSize, ( furthest + 10 ) / 2, 0.1f );
+    }
+}
