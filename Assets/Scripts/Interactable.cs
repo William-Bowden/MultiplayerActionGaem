ec5082b650ac91f17f187b4d6a0405468d0c1368
William-Bowden <william.j.bowden@aol.com>
2019-08-12 01:02:59 -0400

    Starting refactor to Interactables Added weapon stands to Level_2

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour {
+
+    Collider2D col;
+
+    // Start is called before the first frame update
+    void Start() {
+        col = GetComponent<Collider2D>();
+    }
+
+    protected void Interact() {
+        Debug.Log( gameObject + " was interacted with");
+    }
+
+    protected void OnTriggerEnter2D( Collider2D collision ) {
+
+    }
+
+}
